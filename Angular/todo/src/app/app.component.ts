5d1b199e1e5fdc8e300dbc53bd16bce9906881d3
Cristiano Cunha <contato@cristianoprogramador.com>
2019-11-30 12:58:47 -0300

    Implementa a criação de tasks

--- Angular/todo/src/app/app.component.ts
+++ Angular/todo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from '../models/todo.model';
 
 @Component({
@@ -6,30 +7,57 @@ import { Todo } from '../models/todo.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   public todos: Array<Todo> = [];
   public title = 'Minhas tarefas';
+  public form: FormGroup;
 
-  constructor() {
-    this.todos.push(new Todo('C#', true, 0));
-    this.todos.push(new Todo('Angular', false, 1));
-    this.todos.push(new Todo('TypeScript', false, 2));
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose(
+        [Validators.minLength(2),
+        Validators.maxLength(25),
+        Validators.required])]
+    });
+    this.load();
+  }
+
+
+  add() {
+    const tile = this.form.controls.title.value;
+    const id = this.todos.length;
+    this.todos.push(new Todo(tile, false, id));
+    this.clearForm();
+    this.save();
+  }
+
+  clearForm() {
+    this.form.reset();
   }
 
   remove(todo: Todo): void {
     const index = this.todos.indexOf(todo);
     if (index !== -1) {
       this.todos.splice(index, 1);
+      this.save();
     }
   }
 
-
-
   markAsDone(todo: Todo): void {
-    const index = this.todos.indexOf(todo);
-    if (index !== -1) {
-      this.todos[index].done = true;
-    }
+    todo.done = true;
+    this.save();
+  }
+
+  save() {
+    const jsonData = JSON.stringify(this.todos);
+
+    localStorage.setItem('todos', jsonData);
+  }
+
+  load() {
+    const jsonData = localStorage.getItem('todos');
+    this.todos = JSON.parse(jsonData);
   }
 }
 
